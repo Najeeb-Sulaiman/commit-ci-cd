83bc96d562b855763c432f175e5e0a4bfd4af028
Najeeb <sulaimannajeebadesoji@gmail.com>
2026-08-05 21:57:53 +0100

    Added gender cleaning

--- cleaner.py
+++ cleaner.py
@@ -6,8 +6,13 @@ def clean_email(email):
     return email.strip().lower()
 
 
+def clean_gender(gender):
+    return gender.strip().lower()
+
+
 def clean_customer(customer):
     return {
         "name": clean_name(customer["name"]),
         "email": clean_email(customer["email"]),
+        "gender": clean_gender(customer["gender"]),
     }
