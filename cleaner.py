4ceff6f482114f4733004b00f2e0cfa146de77cf
Najeeb <sulaimannajeebadesoji@gmail.com>
2026-08-05 20:48:25 +0100

    first commit

---
+++ cleaner.py
@@ -0,0 +1,13 @@
+def clean_name(name):
+    return name.strip().title()
+
+
+def clean_email(email):
+    return email.strip().lower()
+
+
+def clean_customer(customer):
+    return {
+        "name": clean_name(customer["name"]),
+        "email": clean_email(customer["email"]),
+    }
